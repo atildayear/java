70375b402057af0cf886e599f114cc1af8033b8c
atildayear <yeonah.park97@gmail.com>
2024-11-29 10:45:03 +0900

    변경

--- chap06/src/com/chap06/sub01/Calculator.java
+++ chap06/src/com/chap06/sub01/Calculator.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 //계산 기능을 제공하는 클래스
 public class Calculator {
     //합계 계산하는 메서드
-    double sum(List<BigDecimal>numbers){
+    public double sum(List<BigDecimal> numbers){
         BigDecimal sum = BigDecimal.ZERO; // 합계를 저장할 변수를 초기화
-        for (BigDecimal num:numbers){
+        for (BigDecimal num : numbers){
             sum = sum.add(num);
         }
         return sum.doubleValue(); // double 타입으로 변환하여 변환
